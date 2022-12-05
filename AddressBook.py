19c5986789f699f84a8164a9374bc4541b55d64e
YaroslavZq <svyatikua2@gmail.com>
2022-12-05 21:34:02 +0200

    add requirements, black in dev dependencies

--- AddressBook.py
+++ AddressBook.py
@@ -4,64 +4,63 @@ from re import search, match
 import pickle
 from abc import ABC, abstractmethod
 
-class AddressBook(UserDict):
 
-    def __init__(self, data = None):
+class AddressBook(UserDict):
+    def __init__(self, data=None):
         super().__init__(data)
         self.notes = []
 
     def add_record(self, record):
         self.data[record.name.value] = record
-        return 'Record was added'
+        return "Record was added"
 
     def return_record_by_name(self, name):
         return self.data[name]
 
     def remove_record(self, record):
         del self.data[record.name.value]
-        return 'Record was deleted'
+        return "Record was deleted"
 
     def search_contact(self, name):
         if name in self.data:
             record = self.data[name]
-            return f'{record}'
+            return f"{record}"
         else:
-            return 'Contact not found'
+            return "Contact not found"
 
-
-    def add_note(self, note, description = None, tag = None):
-        self.notes.append(Note(note, description, tag.lower() if tag else ''))
+    def add_note(self, note, description=None, tag=None):
+        self.notes.append(Note(note, description, tag.lower() if tag else ""))
         return f'Note "{note}" was create'
 
     def add_desc_to_note(self, note):
         for book_note in self.notes:
             if not note.startswith(book_note.note):
                 continue
-            description = note[len(book_note.note):].strip()
+            description = note[len(book_note.note) :].strip()
             book_note.description = description
-            return 'description is added'
-        return 'Note not found'
+            return "description is added"
+        return "Note not found"
 
     def add_tag_to_note(self, note):
         for book_note in self.notes:
             if not note.startswith(book_note.note):
                 continue
-            tags = note[len(book_note.note):].strip()
-            lst_tags = book_note.tags.split(',')
-            lst_tags.extend([tag.strip() for tag in tags.lower().split(',')])
+            tags = note[len(book_note.note) :].strip()
+            lst_tags = book_note.tags.split(",")
+            lst_tags.extend([tag.strip() for tag in tags.lower().split(",")])
             lst_tags.sort()
-            book_note.tags = ','.join(tag for tag in lst_tags if tag != '')
-            return 'Tags is added'
-        return 'Note not found'
+            book_note.tags = ",".join(tag for tag in lst_tags if tag != "")
+            return "Tags is added"
+        return "Note not found"
 
     def search_notes_by_tags(self, tags):
         tags_answer = []
-        lst_tags = tags.split(',')
-        lst_tags = [item.lower() for item in lst_tags if item != '']
+        lst_tags = tags.split(",")
+        lst_tags = [item.lower() for item in lst_tags if item != ""]
         for book_note in self.notes:
-            if set(book_note.tags.split(',')) & set(lst_tags) == set(lst_tags):
+            if set(book_note.tags.split(",")) & set(lst_tags) == set(lst_tags):
                 tags_answer.append(book_note)
-        return tags_answer or f'Tags {tags} not found'
+        return tags_answer or f"Tags {tags} not found"
 
     def sort_notes_by_tags(self):
         self.notes = sorted(self.notes, key=lambda x: x.tags)
@@ -70,15 +69,15 @@ class AddressBook(UserDict):
         for book_note in self.notes:
             if book_note.note == note:
                 return book_note
-        return 'Note not found'
+        return "Note not found"
 
     def remove_note(self, note):
         book_note = self.search_notes_by_name(note)
         if book_note:
             self.notes.remove(book_note)
-            return 'Note was removed'
+            return "Note was removed"
         else:
-            return 'Note not found'
+            return "Note not found"
 
     def save_to_file(self, filename):
         with open(filename, "wb") as file:
@@ -90,63 +89,67 @@ class AddressBook(UserDict):
             content = pickle.load(file)
         return content
 
-    def delete_name(self, name):  
+    def delete_name(self, name):
         if name in self.data:
             del self.data[name]
             return True
 
     def show_birthday(self, user_input):
         s = []
-        for name, record in self.data.items(): 
+        for name, record in self.data.items():
             if record.days_to_birthday() == user_input:
-                s.append(name)       
+                s.append(name)
             else:
                 continue
-        return ', '.join(s)
+        return ", ".join(s)
 
-class Record():
-    def __init__(self, name, phone = None):
+
+class Record:
+    def __init__(self, name, phone=None):
         self.name = Name(name)
         self.phones = [Phone(phone)] if phone else []
         self.email = None
         self.birthday = None
         self.address = None
-       
+
     def __repr__(self) -> str:
-        phones = ', '.join([phone.value for phone in self.phones if not phone.value is None])
+        phones = ", ".join(
+            [phone.value for phone in self.phones if not phone.value is None]
+        )
         name = self.name
-        email = self.email or ''
-        birthday = self.birthday.value.strftime('%d.%m.%Y') if self.birthday else ''
-        address = self.address or ''
-        return f'{name} : phones {phones}, email: {email}, birthday: {birthday}, address: {address}'
+        email = self.email or ""
+        birthday = self.birthday.value.strftime("%d.%m.%Y") if self.birthday else ""
+        address = self.address or ""
+        return f"{name} : phones {phones}, email: {email}, birthday: {birthday}, address: {address}"
 
     def add_phone(self, phone):
         for rec_phone in self.phones:
             if rec_phone.value == phone:
-                raise ValueError("""This contact {self.name} 
+                raise ValueError(
+                    """This contact {self.name} 
                             already have phone : {phone}
                             if you want to change tis date try "change_email | new_email"
-                            """)                
+                            """
+                )
         self.phones.append(Phone(phone))
 
-
     def delete_phone(self, phone):
-         for rec_phone in self.phones:
+        for rec_phone in self.phones:
             if not rec_phone.value == phone:
                 continue
             self.phones.remove(rec_phone)
             return True
-    
 
     def add_email(self, email):
         if self.email is None:
             self.email = Email(email)
         else:
-            raise ValueError("""This contact {self.name} 
+            raise ValueError(
+                """This contact {self.name} 
             already have date of email: {self.email}
             if you want to change tis date try "change_email | new_email"
-            """)
-
+            """
+            )
 
     def delete_email(self):
         self.email = None
@@ -155,30 +158,28 @@ class Record():
     def add_birthday(self, birthday):
         if birthday:
             self.birthday = Birthday(birthday)
-            return 'Birthday {self.name.value} is changed'
+            return "Birthday {self.name.value} is changed"
         else:
-            raise('Enter a birthday')
-
+            raise ("Enter a birthday")
 
     def delete_birthday(self):
         self.birthday = None
         return True
 
-
     def delete_address(self):
         self.address = None
         return True
-
 
     def add_address(self, address):
         if self.address is None:
             self.address = Address(address)
         else:
-            raise ValueError("""This contact {self.name} 
+            raise ValueError(
+                """This contact {self.name} 
             already have date of address: {self.address}
             if you want to change tis date try "change_address | new_address"
-            """)
- 
+            """
+            )
 
     def days_to_birthday(self):
         if self.birthday:
@@ -186,11 +187,13 @@ class Record():
             if self.birthday.value.replace(year=today.year) >= today:
                 result = self.birthday.value.replace(year=today.year) - today
             else:
-                result = self.birthday.value.replace(
-                    year=today.year) - today.replace(year=today.year - 1)
+                result = self.birthday.value.replace(year=today.year) - today.replace(
+                    year=today.year - 1
+                )
             return result.days
         else:
-            return 'Empty'        
+            return "Empty"
+
 
 class Field(ABC):
     def __init__(self, value):
@@ -209,26 +212,28 @@ class Field(ABC):
     def value(self, value):
         pass
 
-class Address(Field):
 
+class Address(Field):
     @Field.value.setter
     def value(self, value):
         clean_address = (
-                        value.strip()
-                        .replace("(", "")
-                        .replace(")", "")
-                        .replace("-", "")
-                        .replace(" ", "")
-                        .replace(",", " ")
-                    )
+            value.strip()
+            .replace("(", "")
+            .replace(")", "")
+            .replace("-", "")
+            .replace(" ", "")
+            .replace(",", " ")
+        )
         match_value = search(r"\d{5}\ \м.\w+\ \в.\w+(\d+|\D+)+", clean_address)
         if not match_value:
-            raise ValueError(f"Invalide address format {clean_address}. Address format should be IIII, м.Місто, в.Вулиця, дод.записи")
+            raise ValueError(
+                f"Invalide address format {clean_address}. Address format should be IIII, м.Місто, в.Вулиця, дод.записи"
+            )
         value = str(value)
         self._value = value
 
-class Birthday(Field):
 
+class Birthday(Field):
     @Field.value.setter
     def value(self, value):
         value = value.strip()
@@ -240,50 +245,53 @@ class Birthday(Field):
                 break
 
         if not args or len(args) > 2:
-            raise ValueError("Invalide date format. Date format should be YYYY.MM.DD or DD.MM.YYYY.")
+            raise ValueError(
+                "Invalide date format. Date format should be YYYY.MM.DD or DD.MM.YYYY."
+            )
 
         if int(value) > 31:
             return date(int(value), int(args[0]), int(args[1]))
 
-        value =  date(int(args[1]), int(args[0]), int(value))
+        value = date(int(args[1]), int(args[0]), int(value))
         self._value = value
 
 
 class Phone(Field):
-
     @Field.value.setter
-    def value(self, value:str):
-        if not all((value.startswith('+380'), len(value) == 13, value[1:].isdigit())):
-            raise ValueError(f"""Phone number {value} is not valid, 
-            please enter correcct phone '+380XXXXXXX'""")
+    def value(self, value: str):
+        if not all((value.startswith("+380"), len(value) == 13, value[1:].isdigit())):
+            raise ValueError(
+                f"""Phone number {value} is not valid, 
+            please enter correcct phone '+380XXXXXXX'"""
+            )
         self._value = value
-
 
 
 class Email(Field):
-    
     @Field.value.setter
-    def value(self, value:str):
-        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    def value(self, value: str):
+        regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
         if not (match(regex, value)):
-            raise ValueError(f""""Email {value} is not valid,
+            raise ValueError(
+                f""""Email {value} is not valid,
             please enter correct email.
             Example of emails: my.ownsite@our-earth.org
-                                ankitrai326@gmail.com""")
+                                ankitrai326@gmail.com"""
+            )
         self._value = value
+
 
 class Name(Field):
-
     @Field.value.setter
-    def value(self, value:str):
+    def value(self, value: str):
         self._value = value
 
+
 class Note:
-    def __init__(self, note, description = '', tags= ''):
+    def __init__(self, note, description="", tags=""):
         self.note = note
-        self.description = description if description else ''
-        self.tags = ','.join((tag for tag in tags.split(' '))) if tags else ''
+        self.description = description if description else ""
+        self.tags = ",".join((tag for tag in tags.split(" "))) if tags else ""
 
     def __repr__(self) -> str:
-        return f'{self.note}: {self.description} ({self.tags})'
-          
+        return f"{self.note}: {self.description} ({self.tags})"
--- SortFunc.py
+++ SortFunc.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import sys
 import os
 
+
 def sorting(dir=None):
     if dir is None:
         dir = sys.argv[1]
@@ -25,22 +26,22 @@ def sorting(dir=None):
         os.makedirs(zips_path)
     if not os.path.exists(not_recognize_path):
         os.makedirs(not_recognize_path)
-    
+
     for file in os.listdir(dir):
         file_path = os.path.join(dir, file)
-        if file_path.endswith(('.jpg', '.png', '.png', '.svg', '.jfif', '.gif')):
+        if file_path.endswith((".jpg", ".png", ".png", ".svg", ".jfif", ".gif")):
             new_file = os.path.join(images_path, file)
             os.replace(file_path, new_file)
-        elif file_path.endswith(('.avi', '.mp4', '.mov', '.mkv')):
+        elif file_path.endswith((".avi", ".mp4", ".mov", ".mkv")):
             new_file = os.path.join(videos_path, file)
             os.replace(file_path, new_file)
-        elif file_path.endswith(('.doc', '.docx', '.txt', '.pdf', '.xlxs', '.pptx')):
+        elif file_path.endswith((".doc", ".docx", ".txt", ".pdf", ".xlxs", ".pptx")):
             new_file = os.path.join(docs_path, file)
             os.replace(file_path, new_file)
-        elif file_path.endswith(('.mp3', '.ogg', '.wav', '.amr')):
+        elif file_path.endswith((".mp3", ".ogg", ".wav", ".amr")):
             new_file = os.path.join(musics_path, file)
             os.replace(file_path, new_file)
-        elif file_path.endswith(('.zip', '.gz', '.tar')):
+        elif file_path.endswith((".zip", ".gz", ".tar")):
             new_file = os.path.join(zips_path, file)
             os.replace(file_path, new_file)
         elif os.path.isfile(dir + file):
